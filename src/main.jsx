02264114839b6093726bb9d43972ed9ef2faa402
yassine-hmadou <g172architectehamadouy@gmail.com>
2026-09-03 14:19:58 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './telegram_channel_manager.jsx'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
